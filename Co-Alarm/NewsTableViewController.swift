5e1f56092611070d3f0379c42e1118350ebaf2c7
yunuki <woogie_ing@naver.com>
2020-04-11 23:11:44 +0900

    add : 주석 추가

--- Co-Alarm/NewsTableViewController.swift
+++ Co-Alarm/NewsTableViewController.swift
@@ -15,6 +15,7 @@ class NewsTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //뉴스 api에서 데이터를 fetch
         NetworkController.sharedInstance.fetchNews { (articles) in
             if let articles = articles {
                 self.articles = articles
@@ -27,27 +28,30 @@ class NewsTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-
+    //cell의 개수 동적으로 설정
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return articles.count
     }
 
-    
+    //각 cell을 동적으로 설정
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "articleCell", for: indexPath)
         cell.textLabel?.font = UIFont.systemFont(ofSize: 20)
+        //뉴스 기사의 제목이 HTML형식이어서 String으로 바꿔줌
         cell.textLabel?.text = try? self.articles[indexPath.row].title.strippingHTML()
         return cell
     }
     
+    //cell을 눌렀을 때의 동작
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         guard let url = URL(string: self.articles[indexPath.row].link) else {return}
+        //SFSafariViewController를 이용하여 기사에 대한 링크 페이지를 띄움
         let safariViewController = SFSafariViewController(url: url)
         present(safariViewController, animated: true, completion: nil)
     }
     
+    //cell의 높이 설정
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100
     }
